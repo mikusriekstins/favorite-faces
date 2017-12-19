5c2ac2fe6f8f73f00f7abc8c54d220e1403869c7
Mikus Riekstins <mikus@mikus-mac.local>
2017-12-20 01:29:15 +0200

    First dev set-up

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Set up</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+    // No components yet
+  }
+}
+</script>
+
